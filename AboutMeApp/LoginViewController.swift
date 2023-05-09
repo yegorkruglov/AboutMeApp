07028d369f0dfb2f465f771bb9d3eb2764e56d86
Egor Kruglov <yegorkruglov@gmail.com>
2023-05-09 13:51:23 +0300

    setup appearance, added ib outlets and actions, gradient look

---
+++ AboutMeApp/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  AboutMeApp
+//
+//  Created by Egor Kruglov on 09.05.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func forgotUserNameButtonTapped() {
+    }
+    
+    
+    @IBAction func forgotPasswordButtonTapped() {
+    }
+    
+
+}
+
---
+++ AboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Egor Kruglov on 09.05.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupGradient()
+        
+    }
+    
+    private func setupGradient() {
+        let gradient = CAGradientLayer()
+
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.systemPink.cgColor, UIColor.systemCyan.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        
+        view.layer.insertSublayer(gradient, at: 0)
+    }
+    
+    @IBAction func logOutButtonTapped() {
+    }
+    
+}
+
